0275d6c4d489e3fd842f8dbc026479837af50edd
conniekang <hqk1121@gmail.com>
2021-11-07 21:21:06 -0500

    commit tree 9dd84c02a2d4ffa018e23bff27f4aac2483d1418

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
-
+class Student < User
+    def initialize(knowledge=[])
+        @knowledge = knowledge
+    end
+    def learn(k)
+        @knowledge << k
+    end
+    def knowledge
+        @knowledge
+    end
 end
